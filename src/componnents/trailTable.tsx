e51e0296427a2e4b78dac9f1167ae1691a963710
Julian Saxer <xx1064xx@gmail.com>
2024-05-31 16:01:42 +0200

    Trail api call und styles

--- src/componnents/trailTable.tsx
+++ src/componnents/trailTable.tsx
@@ -1,31 +1,65 @@
+import { useEffect, useState} from "react";
+
 import '../styles/trailTable.css'
+
+interface Trail {
+    trailId: string;
+    location: string;
+    name: string;
+    dateAndTime: string;
+}
 
 function TrailTable() {
 
+    const token = localStorage.getItem('jwt-token');
+    let isInitial = true;
+    const [trails, setTrails] = useState<Trail[]>([]);
+
+    useEffect(() => {
+        const effectFunction = async () => {
+          if (isInitial) {
+            isInitial = false;
+    
+            try {
+                const response = await fetch('https://localhost:7052/api/Trail/getTrail', {
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${token}`,
+                    },
+                  });
+                const data = (await response.json()) as Trail[];
+
+                console.log(data)
+                setTrails(data);
+    
+        
+                } catch (error) {
+                console.error('Error fetching the weather data:', error);
+        
+                }
+          }
+        };
+    
+        effectFunction();
+      }, []);
+
+    
     
 
     return (
-      <div className="background">
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        <div className='divCard'></div>
-        
-        
-    
+      <div>
+        {trails.map((trail) => ( 
+            <div className="background" key={trail.trailId}>
+                <div className="content">
+                <h2>{trail.name}</h2>
+                <p>{trail.location}</p>
+                </div>
+            </div>
+        ))}
       </div>
+        
+        
     );
   };
   
